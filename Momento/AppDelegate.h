534ae871e1ece5eaab8667bf4df694447a8266e0
Cameron Hawkins <camhawkins32@gmail.com>
2012-12-01 11:46:18 -0800

    Initial Commit

---
+++ Momento/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Momento
+//
+//  Created by Cameron Hawkins on 12/1/12.
+//  Copyright (c) 2012 BAMF's. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Momento/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Momento
+//
+//  Created by Cameron Hawkins on 12/1/12.
+//  Copyright (c) 2012 BAMF's. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
