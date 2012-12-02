1e6b2538ad24c9f3a12c1fd0327452013f0cb758
Cameron Hawkins <camhawkins32@gmail.com>
2012-12-01 16:24:44 -0800

    Added network library.

--- Momento/RecordNewViewController.h
+++ Momento/RecordNewViewController.h
@@ -8,10 +8,23 @@
 
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
+#import "AFHTTPClient.h"
+#import "AFHTTPRequestOperation.h"
 
-@interface RecordNewViewController : UIViewController
+@interface RecordNewViewController : UIViewController <AVCaptureVideoDataOutputSampleBufferDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *cameraPreview;
 @property (weak, nonatomic) IBOutlet UIButton *recordButton;
+
+@property (readonly) NSDictionary *videoSettings;
 @property (strong, nonatomic) AVCaptureSession *session;
+@property (nonatomic, strong) AVAssetWriter *assetWriter;
+@property (nonatomic, strong) AVAssetWriterInput *assetWriterInput;
+@property (nonatomic, strong) AVAssetWriterInputPixelBufferAdaptor *pixelBufferAdaptor;
+
+@property (readonly) NSString *defaultFilelocation;
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
+didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
+       fromConnection:(AVCaptureConnection *)connection;
 @end
--- Momento/RecordNewViewController.m
+++ Momento/RecordNewViewController.m
@@ -38,30 +38,118 @@
 {
   [super viewDidLoad];
 	// Do any additional setup after loading the view.
+  [self setup];
 }
 
-- (void)didReceiveMemoryWarning
-{
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (NSString *) defaultFilelocation {
+  NSString *returnString = [NSHomeDirectory()
+                            stringByAppendingPathComponent:@"Documents"];
+  return [returnString stringByAppendingPathComponent:@"DefualtVideoFile.mp4"];
+}
+
+- (NSDictionary *) videoSettings {
+  return [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA], kCVPixelBufferPixelFormatTypeKey, nil];
+}
+
+- (AVAssetWriter *) assetWriter {
+  if (!_assetWriter) {
+    NSURL *newVideoURL = [NSURL fileURLWithPath:self.defaultFilelocation];
+    NSLog(@"Try to write to %@", newVideoURL);
+    NSError *error = nil;
+    _assetWriter = [[AVAssetWriter alloc] initWithURL:newVideoURL
+                              fileType:AVFileTypeMPEG4
+                                 error:&error];
+
+    if (error) {
+      NSLog(@"An error occured creating the asset writer");
+    }
+    [_assetWriter addInput:self.assetWriterInput];
+  }
+  return _assetWriter;
+}
+
+- (AVAssetWriterInput *) assetWriterInput {
+  if (!_assetWriterInput) {
+    NSDictionary *outputSettings = [NSDictionary dictionaryWithObjectsAndKeys:
+                                    [NSNumber numberWithInt:640], AVVideoWidthKey,
+                                    [NSNumber numberWithInt:480], AVVideoHeightKey,
+                                    AVVideoCodecH264, AVVideoCodecKey, nil];
+    _assetWriterInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo
+                                                           outputSettings:outputSettings];
+    _assetWriterInput.expectsMediaDataInRealTime = YES;
+  }
+  return _assetWriterInput;
+}
+
+- (AVAssetWriterInputPixelBufferAdaptor *) pixelBufferAdaptor {
+  if (!_pixelBufferAdaptor) {
+    _pixelBufferAdaptor = [[AVAssetWriterInputPixelBufferAdaptor alloc] initWithAssetWriterInput:self.assetWriterInput
+                                                                     sourcePixelBufferAttributes:self.videoSettings];
+  }
+  return _pixelBufferAdaptor;
 }
 
 - (void) setup {
   self.isRecording = NO;
   [self.recordButton setTitle:@"Start" forState:UIControlStateNormal];
+
+  if (!self.assetWriter || !self.assetWriterInput || !self.pixelBufferAdaptor) {
+    NSLog(@"Error instatiating avasset writer objects");
+  } else {
+    NSLog(@"Setup successful");
+  }
 }
 
 - (IBAction) recordButtonPressed:(UIButton *)sender {
   NSLog(@"Record Button Pressed");
   if (!self.isRecording) {
+    NSLog(@"Starting asset Writer");
+    [self.assetWriter startWriting];
+    [self.assetWriter startSessionAtSourceTime:kCMTimeZero];
+    NSLog(@"Starting capture session");
     [self.session startRunning];
     [self.recordButton setTitle:@"Stop" forState:UIControlStateNormal];
     self.isRecording = YES;
+    NSLog(@"Running...");
   } else {
+    NSLog(@"Stopping capture session");
     [self.session stopRunning];
+    NSLog(@"Stopping Writing");
+    [self.assetWriter finishWritingWithCompletionHandler:^(){
+      NSLog (@"finished writing");
+    }];
+    self.assetWriter = nil;
+    self.assetWriterInput = nil;
     [self.recordButton setTitle:@"ReStart" forState:UIControlStateNormal];
     self.isRecording = NO;
+    NSLog(@"Finished.");
   }
+}
+
+- (IBAction)finishButtonPressed:(id)sender {
+  NSLog(@"Welcome to finish button pressed handler!");
+  if ([[NSFileManager defaultManager] fileExistsAtPath:self.defaultFilelocation])
+  {
+    NSLog(@"file exists");
+  }
+  //NSData *data = [NSData dataWithContentsOfURL:[NSURL fileURLWithPath:self.defaultFilelocation]];
+  NSData *data = [NSData dataWithContentsOfFile:self.defaultFilelocation];
+  NSLog(@"Data length %d", data.length);
+  NSLog(@"Have data, will travel");
+  AFHTTPClient *client = [[AFHTTPClient alloc] initWithBaseURL:[NSURL fileURLWithPath:@"momento.wadec.com/upload"]];
+  NSLog(@"Client Made");
+  NSURLRequest *request = [client multipartFormRequestWithMethod:@"POST" path:@"/upload" parameters:nil constructingBodyWithBlock: ^(id <AFMultipartFormData> formData) {
+    [formData appendPartWithFileURL:[NSURL fileURLWithPath:@"defaultRecordedVideoFile.mp4"] name:@"video" error:nil];
+  }];
+  NSLog(@"URL Request created");
+  
+  AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+  [operation setUploadProgressBlock:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
+    NSLog(@"Sent %lld of %lld bytes", totalBytesWritten, totalBytesExpectedToWrite);
+  }];
+  NSLog(@"Calling operation start");
+  [operation start];
+  NSLog(@"All done.");
 }
 
 - (AVCaptureSession *)session {
@@ -72,7 +160,7 @@
     // getting data from the camera and microphone
     _session = [[AVCaptureSession alloc] init];
     // Setting the desired quality level to HIGH
-    _session.sessionPreset = AVCaptureSessionPresetMedium;
+    self.session.sessionPreset = AVCaptureSessionPresetMedium;
 
     // Now that the session is done, we set up the device we are using, in this case the default device
     // for video media (ie the camera/microphone)
@@ -98,7 +186,7 @@
       NSLog(@"ERROR: trying to open camera: %@", error);
       return nil;
     }
-    [_session addInput:input];
+    [self.session addInput:input];
 
     // Set up the preview
     AVCaptureVideoPreviewLayer *captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_session];
@@ -106,16 +194,29 @@
     [self.cameraPreview.layer addSublayer:captureVideoPreviewLayer];
 
     // Set up out data output
-    //AVCaptureVideoDataOutput *output = [[AVCaptureVideoDataOutput alloc] init];
-    //output.videoSettings = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange], (id)kCVPixelBufferPixelFormatTypeKey, nil];
-
-    // Set up our capture delegate
-    //dispatch_queue_t queue = dispatch_queue_create("myQueue", NULL);
-    //[output setSampleBufferDelegate:self.videoDelegate queue:queue];
-    //[_session addOutput:output];
-    //dispatch_release(queue);
+    AVCaptureVideoDataOutput *output = [[AVCaptureVideoDataOutput alloc] init];
+    [output setVideoSettings:self.videoSettings];
+    
+    dispatch_queue_t queue = dispatch_queue_create("myQueue", NULL);
+    [output setSampleBufferDelegate:self queue:queue];
+    [self.session addOutput:output];
   }
   return _session;
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+  CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+
+  // a very dense way to keep track of the time at which this frame
+  // occurs relative to the output stream, but it's just an example!
+  static int64_t frameNumber = 0;
+  if(self.assetWriterInput.readyForMoreMediaData) {
+    [self.pixelBufferAdaptor appendPixelBuffer:imageBuffer
+                          withPresentationTime:CMTimeMake(frameNumber, 25)];
+    frameNumber++;
+  } else {
+    NSLog(@"Skipping frame");
+  }
 }
 
 @end
